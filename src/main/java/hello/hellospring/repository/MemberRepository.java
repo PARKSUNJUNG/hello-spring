3c89addc897aa2be3a5bcc281a597b1a9f0593d6
PARKSUNJUNG <tjswjd094@gmail.com>
2023-01-03 21:22:30 +0900

    회원 도메인과 리포지토리 만들기 복습

--- src/main/java/hello/hellospring/repository/MemberRepository.java
+++ src/main/java/hello/hellospring/repository/MemberRepository.java
@@ -7,9 +7,8 @@ import java.util.Optional;
 
 public interface MemberRepository {
 
-    Member save(Member member);  // 회원 저장
-    Optional<Member> findById(Long id); // 아이디로 회원 찾기
-    Optional<Member> findByName(String name); // 이름으로 회원 찾기
-    List<Member> findAll(); // 모든 회원의 리스트를 반환
-
+    Member save(Member member); // 회원 저장
+    Optional<Member> findById(Long id); // 아이디 찾기
+    Optional<Member> findByName(String name); // 이름으로 찾기
+    List<Member> findAll(); // 모든 회원 리스트 반환
 }
--- src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
+++ src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
@@ -5,18 +5,15 @@ import hello.hellospring.domain.Member;
 import java.util.*;
 
 // 구현체 만들기
-public class MemoryMemberRepository implements MemberRepository {
+public class MemoryMemberRepository implements MemberRepository{
 
-    // 저장
-    // <id, 값>
     private static Map<Long, Member> store = new HashMap<>();
-    private static long sequence = 0L; // 키값을 생성해주는 변수
-
+    private static long sequence = 0L;
 
     @Override
     public Member save(Member member) {
-        member.setId(++sequence); // 아이디 세팅
-        store.put(member.getId(), member); // store에 멤버 저장
+        member.setId(++sequence);
+        store.put(member.getId(), member);
         return member;
     }
 
@@ -29,15 +26,12 @@ public class MemoryMemberRepository implements MemberRepository {
     public Optional<Member> findByName(String name) {
         return store.values().stream()
                 .filter(member -> member.getName().equals(name))
-                .findAny();  // 람다 사용. 루프로 돌림. 파라미터로 넘어온 name이랑 같으면 반환을 한다.
+                .findAny();
     }
 
     @Override
     public List<Member> findAll() {
         return new ArrayList<>(store.values());
     }
-
-    public void clearStore() {
-        store.clear();
-    }
 }
+
